4a254b097e8323e5847fc17ececd83d6e257e944
RushBComrades <hawkboy1111@yahoo.com>
2017-02-01 10:19:09 -0800

    addten done

---
+++ MyFile.py
@@ -0,0 +1,10 @@
+  # AddTen(n):  Adds 10 to the parameter n and returns the result
+def addTen(n):
+ n = n + 10
+ return n
+print(addTen(100))
+  # DrawRectangle(Anyturtle, l, w):  Self Explanitory
+
+  # DrawPoly(Anyturtle, n):  Will draw a regular polygon with 'n' sides of
+     # * You should select the size of the polygon so that it always fits in the screen
+#**15 points MAX***
