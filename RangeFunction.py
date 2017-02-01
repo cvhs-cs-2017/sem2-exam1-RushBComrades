9e183673e259a317cab08e6331f4004fec188542
RushBComrades <hawkboy1111@yahoo.com>
2017-02-01 11:04:02 -0800

    range 1 done

--- RangeFunction.py
+++ RangeFunction.py
@@ -1,9 +1,11 @@
 """Use the range function to print the numbers from 40 to 1 (backwards)"""
-
+x = 40
+for i in range(39):
+    x = x - 1
+    print(x)
 
 
 """Repeat the exercise but count by 5's"""
-
 
 
 """Write a program that will count print all the multiples of (n) where n is
