95f1f31077a638b04aaed57cea81b1de939623cf
RushBComrades <hawkboy1111@yahoo.com>
2017-02-01 10:26:20 -0800

    drawrectangle done

--- MyFile.py
+++ MyFile.py
@@ -4,7 +4,15 @@ def addTen(n):
  return n
 print(addTen(100))
   # DrawRectangle(Anyturtle, l, w):  Self Explanitory
-
+import turtle
+import time
+po = turtle.Turtle()
+def DrawRectangle(Anyturtle, l, w):
+    for x in range(4):
+        Anyturtle.forward(l)
+        Anyturtle.right(90)
+        Anyturtle.forward(w)
+DrawRectangle(po, 5, 5)
   # DrawPoly(Anyturtle, n):  Will draw a regular polygon with 'n' sides of
      # * You should select the size of the polygon so that it always fits in the screen
 #**15 points MAX***
