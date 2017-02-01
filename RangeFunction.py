212869a8f9b56503ff162f1ae24ed86260c7cbf4
RushBComrades <hawkboy1111@yahoo.com>
2017-02-01 11:06:20 -0800

    by 5 done

--- RangeFunction.py
+++ RangeFunction.py
@@ -7,6 +7,14 @@ for i in range(39):
 
 """Repeat the exercise but count by 5's"""
 
+def by5():
+    y = 40
+    while y > 0:
+        if y % 5 == 0:
+            print (y)
+        y = y - 1
+by5()
+
 
 """Write a program that will count print all the multiples of (n) where n is
 taken from user input.  Include necessary print statements."""
