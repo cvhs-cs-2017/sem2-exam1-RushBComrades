f0fe88d1dc3602e11b233fd0bed2cd5cc8197262
RushBComrades <hawkboy1111@yahoo.com>
2017-02-01 11:11:01 -0800

    encrypt 1 done

--- Encrypt.py
+++ Encrypt.py
@@ -3,11 +3,15 @@
 'Computer Science Makes the World go round but it doesn't make the world round itself!'
 
 Print the save the result as the variable = NoVowels
-""""
-
-
-
-
-
+"""
+def Encrypt(o):
+    vow = "aeiouAEIOU"
+    word = ""
+    for x in o:
+        if x in vow:
+            x = " "
+        word += x
+    return word
+print(Encrypt("Computer Science Makes the World go round but it doesn't make the world round itself!"))
 
 """Write an encryption code that you make up and run it for the variable NoVowels"""
